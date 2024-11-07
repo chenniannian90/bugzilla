dcc32337d8ce9e07786dc8a1e7e71e5dd7f6d4d1
niannian.chen <niannian.chen@denglinai.com>
2024-11-07 15:18:05 +0800

    feat: add

--- extensions/Fork/Extension.pm
+++ extensions/Fork/Extension.pm
@@ -37,6 +37,7 @@ sub install_update_db {
         $field = Bugzilla::Field->create({
             name        => 'fork_bug_id',
             description => 'ForkBugID',
+            custom => 1,
             enter_bug   => 1,
             # custom      => 1,
             # enter_bug   => 1,         # 在创建 Bug 时可见
